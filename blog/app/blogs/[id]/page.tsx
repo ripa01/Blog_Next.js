ebd06dfbb430252b6e09199b3075ddc7a3c2829c
ripa01 <ripabiswas2739@gmail.com>
2025-03-23 23:50:04 +0600

    refactor BlogPage component to handle params as a promise and add navigation link to home

--- blog/app/blogs/[id]/page.tsx
+++ blog/app/blogs/[id]/page.tsx
@@ -1,12 +1,16 @@
 import { getBlogById } from "@/app/utils/single-blog";
 import { Blog } from "@/app/globals/types";
+import Link from "next/link";
 
-interface BlogPageProps {
-  params: { id: number };
-}
 
-export default async function BlogPage({ params }: BlogPageProps) {
-  const blog: Blog | null = await getBlogById(Number(params.id));
+// interface BlogPageProps {
+//   params: { id: number };
+// }
+
+export default async function BlogPage({ params }: {params: Promise<{ id: number }> }) {
+  const {id} = await params;
+  const blog: Blog | null = await getBlogById(id);
+  // const blog: Blog | null = await getBlogById(Number(params.id));
 
   if (!blog) {
     return <p className="text-center text-red-500">Blog not found</p>;
@@ -20,6 +24,9 @@ export default async function BlogPage({ params }: BlogPageProps) {
       </p>
       <p className="text-gray-600 mt-2 italic">Category: {blog.category}</p>
       <div className="mt-6">{blog.content}</div>
+      <Link href="/" className="mt-4 inline-block text-blue-600 font-medium">
+        Go back to Home →
+      </Link>
     </div>
   );
 }
